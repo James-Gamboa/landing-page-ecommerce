2698198340977104e7b824d158a9c313aeea50f4
James Guevara Gamboa <89418909+James-Gamboa@users.noreply.github.com>
2024-10-15 21:02:56 -0600

    update header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
 
   return (
     <header className="text-white">
-      <div className="flex justify-between items-center px-4 py-2 bg-custom-high-gray text-sm h-20">
+      <div className="flex justify-between items-center px-4 py-2 bg-custom-high-gray text-sm h-16">
         <span className="mx-4">Welcome to Needus & Get the best product</span>
         <div className="flex items-center space-x-4 mx-4">
           <button className="flex items-center">
@@ -22,32 +22,34 @@ export default function Header() {
         </div>
       </div>
       <div className="bg-white p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <div className="flex w-40">
-            <Image
-              src="/img/logo-needus.png"
-              alt="Logo Needus"
-              width={100}
-              height={100}
-            />
-          </div>
-          <div className="flex items-center relative">
-            <input
-              type="text"
-              placeholder="Search Products"
-              className="p-2 outline-none w-full text-black border border-gray-300 rounded-l-md"
-            />
-            <Image
-              src="/img/search.png"
-              alt="Search Icon"
-              width={35}
-              height={35}
-              className="p-2 bg-custom-high-gray rounded-r-md border-2 w-[11%]"
-            />
+        <div className="container mx-auto flex flex-col items-center">
+          <div className="flex items-center justify-center w-full">
+            <div className="flex w-40 mr-11">
+              <Image
+                src="/img/logo-needus.png"
+                alt="Logo Needus"
+                width={100}
+                height={100}
+              />
+            </div>
+            <div className="flex items-center relative mx-4">
+              <input
+                type="text"
+                placeholder="Search Products"
+                className="p-4 w-[300px] outline-none text-black border border-gray-300 rounded-l-md" 
+              />
+              <Image
+                src="/img/search.png"
+                alt="Search Icon"
+                width={40} 
+                height={40} 
+                className="p-3 bg-custom-high-gray rounded-r-md border-2 w-[16%] h-full" 
+              />
+            </div>
             <div
               onMouseEnter={() => setShowSubmenu(true)}
               onMouseLeave={() => setShowSubmenu(false)}
-              className="flex items-center px-4 py-2 text-black cursor-pointer relative"
+              className="flex items-center text-black cursor-pointer relative mx-4"
             >
               <span className="mr-1 whitespace-nowrap">All Categories</span>
               <FaChevronDown className="text-black" />
@@ -64,23 +66,23 @@ export default function Header() {
                   ))}
                 </div>
               )}
+            </div>
+            <div className="flex items-center space-x-4 text-black mx-2"> 
               <Link href="/">Login</Link>
               <Link href="/">Signup</Link>
+              <Image
+                src="/img/favorite.png"
+                alt="Favorite Icon"
+                width={20}
+                height={20}
+              />
+              <Image
+                src="/img/shopcard-logo.png"
+                alt="Shopping Cart Icon"
+                width={30}
+                height={30}
+              />
             </div>
-          </div>
-          <div className="flex items-center space-x-4 text-black">
-            <Image
-              src="/img/favorite.png"
-              alt="Favorite Icon"
-              width={20}
-              height={20}
-            />
-            <Image
-              src="/img/shopcard-logo.png"
-              alt="Shopping Cart Icon"
-              width={30}
-              height={30}
-            />
           </div>
         </div>
       </div>
